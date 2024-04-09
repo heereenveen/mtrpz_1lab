a8e062afdefd800003d02eea98c258e5993a299e
Rostyslav Hundarev <r.hundarev.im21@kpi.ua>
2024-04-09 13:39:55 +0300

    Markdown to HTML conversion by adding support for bold, italic, and inline code

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import argparse
 import sys
 import re
 
+def convert_paragraphs_to_html(markdown_text):
+    markdown_text = re.sub(r'\*\*(.*?)\*\*', r'<strong>\1</strong>', markdown_text)
+    markdown_text = re.sub(r'_(.*?)_', r'<em>\1</em>', markdown_text)
+    markdown_text = re.sub(r'(?<!`)(?!```)`([^`]+)`(?!```)', r'<code>\1</code>', markdown_text)
+    return markdown_text
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', metavar='input_file', type=str)
