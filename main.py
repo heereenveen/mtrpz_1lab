9200f7af0ee39a1aca76fc87b43cff88413806cc
Rostyslav Hundarev <r.hundarev.im21@kpi.ua>
2024-04-09 13:35:52 +0300

    Added processing of command line arguments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import argparse
 import sys
 import re
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', metavar='input_file', type=str)
+    parser.add_argument('--out', dest='output_file', metavar='output_file', type=str)
+    args = parser.parse_args()
